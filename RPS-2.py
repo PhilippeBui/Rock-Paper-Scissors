a8e5c63e0c621a00193cbe4ad578a969e6719887
PhilippeBui <philbui91@gmail.com>
2019-05-22 18:30:47 -0700

    Create RPS-2.py

---
+++ RPS-2.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+#simple rock/paper/scissors game
+
+from random import randint
+
+player = input("r-rock, p-paper, s-scissors")
+
+cchoice= randint(1,3)
+
+if cchoice == 1:
+  computer= 'r'
+elif cchoice == 2:
+  computer= 'p'
+else:
+  computer= 's'
+  
+print("player:", player, " vs computer:", computer )
+  
+  
+if player =='r' and computer == 1:
+  print("tie")
+  
+elif player =="r" and computer == 2:
+  print("computer wins")
+
+elif player =="r" and computer == 3:
+  print("player wins")
+    
+elif player =="p" and computer == 1:
+  print("player wins")
+
+elif player =="p" and computer == 2:
+  print("tie")
+
+elif player == "p" and computer == 3:
+  print("computer wins")
+  
+elif player =="s" and computer == 1:
+  print("computer wins")
+
+elif player =="s" and computer == 2:
+  print("player wins")
+
+else:
+  print("tie") 
