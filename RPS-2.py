f4380ca546d141c90bf0363872f7c9d214775029
PhilippeBui <philbui91@gmail.com>
2019-05-22 18:33:31 -0700

    Update RPS-2.py

--- RPS-2.py
+++ RPS-2.py
@@ -1,12 +1,16 @@
 #!/bin/python3
 #simple rock/paper/scissors game
 
+#import random function
 from random import randint
 
+#store player's input into var pchoice
 player = input("r-rock, p-paper, s-scissors")
 
+#store Computer's random integer between 1-3 into var cchoice
 cchoice= randint(1,3)
 
+#store computer's choice as string
 if cchoice == 1:
   computer= 'r'
 elif cchoice == 2:
@@ -14,9 +18,10 @@ elif cchoice == 2:
 else:
   computer= 's'
   
+#print player and computer's decisions
 print("player:", player, " vs computer:", computer )
   
-  
+#compare each choice and print result of winner or tie
 if player =='r' and computer == 1:
   print("tie")
   
